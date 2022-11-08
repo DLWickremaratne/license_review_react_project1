89c5a7b4607c14c8ff16b0b764d8ab365a045c86
DLWickremeratne <dhanithlak@gmail.com>
2022-11-08 18:38:44 +0530

    URL parameters

---
+++ src/pages/drivers-content.js
@@ -0,0 +1,20 @@
+const drivers = [
+    {
+        id: 1,
+        fullName :"Dhanith Lakjaya",
+        branch : "Kandy",
+        gender: "male",
+        teamName: "A"
+    },
+    {
+        id: 1,
+        fullName :"Pradeep",
+        branch : "Kandy",
+        gender: "male",
+        teamName: "A"
+    },
+    
+    
+];
+
+export default drivers;
